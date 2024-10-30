83119cf774a6b361decddd66597f9007d813a9de
csk2q <143836825+csk2q@users.noreply.github.com>
2024-10-30 12:59:09 -0500

    Move oldAlgorithms into new project

---
+++ GraphAlgorithms/DataStructures/NeoNode.cs
@@ -0,0 +1,11 @@
+namespace Ai1_Search_Methods;
+
+public class NeoNode
+{
+    /* TODO Make a node with the following features:
+     * Name
+     * Links to other nodes
+     * Position?
+     * Cost?
+     */
+}
