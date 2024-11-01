f70e178beb06e829924fb791575170dfc33c8f50
csk2q <143836825+csk2q@users.noreply.github.com>
2024-11-01 12:23:31 -0500

    Remove dead code

--- GraphAlgorithms/Logic/GraphGenerator.cs
+++ GraphAlgorithms/Logic/GraphGenerator.cs
@@ -16,7 +16,6 @@ public class GraphGenerator
         
         
     }
-
     
     /*
      * https://stackoverflow.com/questions/39112540/randomly-connected-graph-generator
@@ -69,80 +68,6 @@ public class GraphGenerator
             toBeFrozenNodes.Add(completeNode.Name, completeNode.Cost);
 
 
-        
-        return new Graph(toBeFrozenNodes.ToFrozenDictionary(), adjMatrix);
-    }
-    
-    /* TODO rewrite this because the max distance is three between all nodes. */
-    public static Graph OLDGenerateRandomGraph(int seed, int nodeCount,
-        int minEdges, int maxEdges,
-        int minCost, int maxCost)
-    {
-        var rand = new Random(seed);
-        var nodes = new NeoNode[nodeCount];
-
-        // Create nodes
-        for (var i = 0; i < nodeCount; i++)
-        {
-            nodes[i] = new NeoNode(i.ToString(), rand.Next(minCost, maxCost));
-        }
-
-        // Create undirected/symmetric adjacency matrix
-        AdjacencyMatrix<string> adjMatrix = new(nodes.Select(n => n.Name).ToArray(), true);
-
-        // Create a minimum spanning tree from the existing nodes
-        for (var i = 1; i < nodeCount; i++)
-        {
-            var randPreviousNodeIndex = rand.Next(0, i);
-            adjMatrix.SetAdjacency(nodes[i].Name, nodes[randPreviousNodeIndex].Name, true);
-        }
-
-        List<NeoNode> incompleteNodes = new (nodes);
-        List<NeoNode> completeNodes = new (nodeCount);
-
-        while (incompleteNodes.Count > 0)
-        {
-            // Pop node from list
-            var node = incompleteNodes[incompleteNodes.Count - 1];
-            incompleteNodes.RemoveAt(incompleteNodes.Count - 1);
-
-            if (adjMatrix.CountEdgesFrom(node.Name) < maxEdges)
-            {
-                var localMaxEdges = (int)Math.Max(Math.Ceiling(Math.Sqrt(maxEdges + 1)), minEdges);
-                var targetEdgeCount = rand.Next(minEdges, localMaxEdges);
-                int currentEdgeCount = adjMatrix.CountEdgesFrom(node.Name);
-                int attemptCount = 0;
-                int maxAttempts = nodeCount * 2;
-
-                while (currentEdgeCount - targetEdgeCount > 0 && incompleteNodes.Count > 0 &&
-                       attemptCount < maxAttempts)
-                {
-                    var otherNodeIndex = rand.Next(0, incompleteNodes.Count);
-                    attemptCount++;
-
-                    // Prevent adding edges to nodes that already have the maximum number of edges
-                    // Prevent adding an edge that already exists
-                    if (adjMatrix.CountEdgesFrom(incompleteNodes[otherNodeIndex].Name) >= maxEdges
-                        || adjMatrix.GetAdjacency(node.Name, incompleteNodes[otherNodeIndex].Name))
-                        continue;
-
-                    adjMatrix.AddAdjacency(node.Name, incompleteNodes[otherNodeIndex].Name);
-                    currentEdgeCount++;
-                    if(DEBUG_Prints)
-                        Console.WriteLine($"Attempt count: {attemptCount}");
-                    attemptCount = 0;
-                }
-
-                if (DEBUG_Prints && attemptCount > 0)
-                    Console.WriteLine($"Abandoned due to attempt count: {attemptCount}");
-            }
-
-            completeNodes.Add(node);
-        }
-
-        Dictionary<string, int> toBeFrozenNodes = new (completeNodes.Count());
-        foreach (var completeNode in completeNodes)
-            toBeFrozenNodes.Add(completeNode.Name, completeNode.Cost);            
         
         return new Graph(toBeFrozenNodes.ToFrozenDictionary(), adjMatrix);
     }
--- GraphAlgorithms/Program.cs
+++ GraphAlgorithms/Program.cs
@@ -18,64 +18,6 @@ class Program
         
         Console.WriteLine("Exiting...");
     }
-    
-    /*static void _Main(string[] args)
-    {
-        Console.WriteLine("Generating Graph...!");
-        // Note best first only seems to work for this set of values some how.
-        var graph = GraphGenerator.OLDGenerateRandomGraph(1234, 1000, 1, 20, 1, 10);
-        // var graph = GraphGenerator.OLDGenerateRandomGraph(1234, 500, 1, 3, 1, 10);
-        Console.WriteLine("Graph Generated!");
-
-        var pathLengths = new int[1000];
-
-        /*Parallel.For(0, pathLengths.Length, i =>
-        {
-            Parallel.For(0, pathLengths.Length, j =>
-            {
-                // Console.WriteLine("Starting best first search...");
-                // var resultPath = new BestFirstSearch().RunSearch(i.ToString(), j.ToString(), graph);
-                // Console.WriteLine($"Best First Search Result: {string.Join("->", resultPath)}");
-                pathLengths[i] = new BestFirstSearch().RunSearch(i.ToString(), j.ToString(), graph).Length;
-            });
-            Console.WriteLine("Finished i=" + i);
-        });#1#
-        
-        Random rnd = new Random(100);
-
-        var searchCount = 100;
-        for (var i = 0; i < searchCount; i++)
-        {
-            var x = rnd.Next(0, graph.nodeAndCost.Count);
-            
-            for (var j = 0; j < searchCount; j++)
-            {
-                var y = rnd.Next(0, graph.nodeAndCost.Count);
-
-                
-                pathLengths[i] = new BreathFirstSearch().RunSearch(x.ToString(), y.ToString(), graph).Length;
-            }
-
-            Console.WriteLine("Finished i=" + i + $" {pathLengths[i]}");
-        }
-
-        Console.WriteLine($"Max path length: {pathLengths.Max()}");
-        Dictionary<int, int> pathLengthBuckets = new Dictionary<int, int>();
-
-        foreach (var pathLength in pathLengths)
-        {
-            if (pathLengthBuckets.ContainsKey(pathLength))
-                pathLengthBuckets[pathLength]++;
-            else
-                pathLengthBuckets.Add(pathLength, 1);
-        }
-
-        foreach (var pathLength in pathLengthBuckets)
-            Console.WriteLine($"{pathLength.Key}: {pathLength.Value}");
-
-
-        Console.WriteLine("Exiting...");
-    }*/
 }
 
 [MemoryDiagnoser]
