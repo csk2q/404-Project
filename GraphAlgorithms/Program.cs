54fb11a07956829b3e84c4f859ff4b67ae6e63c6
csk2q <143836825+csk2q@users.noreply.github.com>
2024-11-01 11:18:33 -0500

    Add benchmark

--- GraphAlgorithms/Program.cs
+++ GraphAlgorithms/Program.cs
@@ -1,4 +1,6 @@
-﻿using GraphAlgorithms;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using GraphAlgorithms;
 using GraphAlgorithms.DataStructures;
 using GraphAlgorithms.Logic;
 using GraphAlgorithms.SearchAlgorithms;
@@ -8,6 +10,12 @@ namespace GraphAlgorithms;
 class Program
 {
     static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<MyBenchmark>();
+        
+    }
+    
+    static void _Main(string[] args)
     {
         Console.WriteLine("Generating Graph...!");
         // Note best first only seems to work for this set of values some how.
@@ -28,12 +36,90 @@ class Program
             });
             Console.WriteLine("Finished i=" + i);
         });*/
+        
+        Random rnd = new Random(100);
 
-        for (var i = 0; i < pathLengths.Length; i++)
+        var searchCount = 100;
+        for (var i = 0; i < searchCount; i++)
         {
-            for (var j = 0; j < pathLengths.Length; j++)
+            var x = rnd.Next(0, graph.nodeAndCost.Count);
+            
+            for (var j = 0; j < searchCount; j++)
             {
-                pathLengths[i] = new BreathFirstSearch().RunSearch(i.ToString(), j.ToString(), graph).Length;
+                var y = rnd.Next(0, graph.nodeAndCost.Count);
+
+                
+                pathLengths[i] = new BreathFirstSearch().RunSearch(x.ToString(), y.ToString(), graph).Length;
+            }
+
+            Console.WriteLine("Finished i=" + i + $" {pathLengths[i]}");
+        }
+
+        Console.WriteLine($"Max path length: {pathLengths.Max()}");
+        Dictionary<int, int> pathLengthBuckets = new Dictionary<int, int>();
+
+        foreach (var pathLength in pathLengths)
+        {
+            if (pathLengthBuckets.ContainsKey(pathLength))
+                pathLengthBuckets[pathLength]++;
+            else
+                pathLengthBuckets.Add(pathLength, 1);
+        }
+
+        foreach (var pathLength in pathLengthBuckets)
+            Console.WriteLine($"{pathLength.Key}: {pathLength.Value}");
+
+
+        Console.WriteLine("Exiting...");
+    }
+}
+
+public class MyBenchmark
+{
+    private Graph graph;
+    public MyBenchmark()
+    {
+        Console.WriteLine("Generating Graph...!");
+
+        // Note best first only seems to work for this set of values some how.
+        // var graph = GraphGenerator.OLDGenerateRandomGraph(1234, 500, 1, 3, 1, 10);
+        graph = GraphGenerator.OLDGenerateRandomGraph(1234, 1000, 1, 20, 1, 10);
+        Console.WriteLine("Graph Generated!");
+
+    }
+    
+    [Benchmark]
+    public void Main()
+    {
+
+
+        var pathLengths = new int[1000];
+
+        /*Parallel.For(0, pathLengths.Length, i =>
+        {
+            Parallel.For(0, pathLengths.Length, j =>
+            {
+                // Console.WriteLine("Starting best first search...");
+                // var resultPath = new BestFirstSearch().RunSearch(i.ToString(), j.ToString(), graph);
+                // Console.WriteLine($"Best First Search Result: {string.Join("->", resultPath)}");
+                pathLengths[i] = new BestFirstSearch().RunSearch(i.ToString(), j.ToString(), graph).Length;
+            });
+            Console.WriteLine("Finished i=" + i);
+        });*/
+        
+        Random rnd = new Random(100);
+
+        var searchCount = 100;
+        for (var i = 0; i < searchCount; i++)
+        {
+            var x = rnd.Next(0, graph.nodeAndCost.Count);
+            
+            for (var j = 0; j < searchCount; j++)
+            {
+                var y = rnd.Next(0, graph.nodeAndCost.Count);
+
+                
+                pathLengths[i] = new BreathFirstSearch().RunSearch(x.ToString(), y.ToString(), graph).Length;
             }
 
             Console.WriteLine("Finished i=" + i + $" {pathLengths[i]}");
