d8aa79d7578fc1f7d2a561edbffa3b7a8a4f97b3
csk2q <143836825+csk2q@users.noreply.github.com>
2024-11-01 12:33:23 -0500

    Add DFS benchmark & cleanup

--- GraphAlgorithms/Program.cs
+++ GraphAlgorithms/Program.cs
@@ -14,8 +14,8 @@ class Program
         var summary = BenchmarkRunner.Run<MyBenchmark>();
 
         // new MyBenchmark().RunBenchmark();
-        
-        
+
+
         Console.WriteLine("Exiting...");
     }
 }
@@ -24,27 +24,33 @@ class Program
 public class MyBenchmark
 {
     private const int Seed = 1245;
-    
+    Random rnd = new Random(Seed);
+    string x;
+    string y;
+
     private readonly Graph graph;
+
     public MyBenchmark()
     {
         Console.WriteLine("Generating Graph...!");
 
         graph = GraphGenerator.GenerateRandomGraph(1234, 1000, 250, 1, 10);
         Console.WriteLine("Graph Generated!");
+
+        Random rnd = new Random(Seed);
+        x = rnd.Next(0, graph.nodeAndCost.Count).ToString();
+        y = rnd.Next(0, graph.nodeAndCost.Count).ToString();
     }
 
     [Benchmark]
     public void BreathFirstSearch()
     {
-        Random rnd = new Random(Seed);
-        var x = rnd.Next(0, graph.nodeAndCost.Count);
-        var y = rnd.Next(0, graph.nodeAndCost.Count);
-        
-        _ = new BreathFirstSearch().RunSearch(x.ToString(), y.ToString(), graph);
+        _ = new BreathFirstSearch().RunSearch(x, y, graph);
     }
-    
-    
 
-    
+    [Benchmark]
+    public void DepthFirstSearch()
+    {
+        _ = new DepthFirstSearch().RunSearch(x, y, graph);
+    }
 }
